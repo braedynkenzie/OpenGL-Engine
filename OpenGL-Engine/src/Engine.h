49c8e11ad2f0442250db5810a9383c972bbe5d26
Braedyn Kenzie <braedyn26@gmail.com>
2020-08-09 18:35:52 -0700

    Added own keycodes instead of having to inlcude GLFW in the engine client application

--- OpenGL-Engine/src/Engine.h
+++ OpenGL-Engine/src/Engine.h
@@ -7,6 +7,10 @@
 #include "Engine\Layer.h"
 #include "Engine\Log.h"
 
+#include "Engine\Input.h"
+#include "Engine\KeyCodes.h"
+#include "Engine\MouseButtonCodes.h"
+
 #include "Engine\Imgui\ImGuiLayer.h"
 
 // ---- Engine Entry Point --------
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -10,11 +10,15 @@ public:
 	void OnUpdate() override
 	{
 		// ENGINE_INFO("Example layer OnUpdate called!");
+
+		// Testing input polling and OpenGL-Engine keycodes
+		if (Engine::Input::IsKeyPressed(KEY_TAB))
+			ENGINE_INFO("Tab key is currently pressed!");
 	}
 
 	void OnEvent(Engine::Event& event) override
 	{
-		ENGINE_TRACE("Event passed to Example Layer: {0}", event);
+		// ENGINE_TRACE("Event passed to Example Layer: {0}", event);
 	}
 };
 
